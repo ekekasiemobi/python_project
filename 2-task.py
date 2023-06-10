441263be1bd291a4ed8e0db3a1bf735d7bb978f6
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-10 12:53:27 +0100

    task2

---
+++ 2-task.py
@@ -0,0 +1,16 @@
+def replace_in_list(my_list, idx, element):
+    list_count = len(my_list)
+    if idx < 0:
+        return my_list
+    if idx > list_count:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = replace_in_list(my_list, idx, new_element)
+print(new_list)
+print(my_list)
