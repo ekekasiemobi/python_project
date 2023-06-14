6b54d10f8467d9d2601c5f72ccf36ab15c1e9968
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-14 16:59:12 +0100

    task10

---
+++ set_dictionary/10-task.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if type(a_dictionary) is not dict or len(a_dictionary) == 0:
+        return None
+    else:
+        max = list(a_dictionary.keys())[0]
+        for key in a_dictionary:
+            if a_dictionary[key] > a_dictionary[max]:
+                max = key
+        return max
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
